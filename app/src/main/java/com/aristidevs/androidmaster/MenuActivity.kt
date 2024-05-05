004e04659267911be753838aad4a7b254a678b84
pablo <tonyp2hoy@gmail.com>
2024-05-05 03:01:49 -0400

    feat: app IMC first gender components

---
+++ app/src/main/java/com/aristidevs/androidmaster/MenuActivity.kt
@@ -0,0 +1,33 @@
+package com.aristidevs.androidmaster
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aristidevs.androidmaster.imccalculator.ImcCalculatorActivity
+import firstapp.FirstAppActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener {navigateToSaludApp()}
+        btnImcApp.setOnClickListener {navigateToImcApp()}
+    }
+
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludApp(){
+        val intent = Intent(this,FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+}
