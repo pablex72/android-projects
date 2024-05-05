e0b9104744be9efe91fbf2501885228e5e5189bd
pablo <tonyp2hoy@gmail.com>
2024-05-04 22:05:28 -0400

    feat: android projects init

---
+++ app/src/main/java/firstapp/FirstAppActivity.kt
@@ -0,0 +1,28 @@
+package firstapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aristidevs.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            Log.i("hellllo","button pulsado")
+        }
+        //AL ARRANCAR LA PANTALLA
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
