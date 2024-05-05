b12faad90e36f1cb8aebeeed226593647453bc5a
pablo <tonyp2hoy@gmail.com>
2024-05-04 23:02:13 -0400

    feat: second app showing new window

--- app/src/main/java/firstapp/FirstAppActivity.kt
+++ app/src/main/java/firstapp/FirstAppActivity.kt
@@ -1,10 +1,12 @@
 package firstapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aristidevs.androidmaster.R
@@ -15,8 +17,17 @@ class FirstAppActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+
         btnStart.setOnClickListener {
-            Log.i("hellllo","button pulsado")
+            val name = etName.text.toString()
+            if(name.isNotEmpty())
+            { Log.i("hellllo", "button pulsado $name")
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
         }
         //AL ARRANCAR LA PANTALLA
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
